abaf73acdacfb53ae536900d72db03b7d04b70d9
limadanilo93 <88122215+limadanilo93@users.noreply.github.com>
2022-02-21 22:25:39 -0300

    feat:! finsl additions - Conditionals

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ const teclas = document.querySelectorAll('.tecla');
 const audioInstrumentos = document.querySelectorAll('audio');
 
 for (let i = 0; i <teclas.length; i++) {
-    function tocaAudio(){
+
+    teclas[i].onclick = function(){
         audioInstrumentos[i].play();
-     }
-    teclas[i].onclick = tocaAudio;
+    }
+
+    teclas[i].onkeydown = function(evento){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            teclas[i].classList.add('ativa');
+        }
+        
+    }
+    teclas[i].onkeyup = function(){
+        teclas[i].classList.remove('ativa');
+    }
+   
 }
 
 console.log(audioInstrumentos);
+//template string `string$(elementodecodigo)`
